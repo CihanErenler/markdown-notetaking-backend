ae43b49786f104c1a3b0f3642fcf57287735aae8
cihanerenler <cihanerenler@outlook.com>
2022-12-26 13:56:47 +0200

    add create folder and create file

--- src/controllers/mainController.mjs
+++ src/controllers/mainController.mjs
@@ -2,41 +2,84 @@ import User from "../models/User.mjs";
 import Code from "../models/Code.mjs";
 
 export const getData = async (req, res, next) => {
-	try {
-		const user = await User.find({ email: req.body.email });
-		const [{ data }] = user;
-		res.status(200);
-		res.json(data);
-	} catch (error) {
-		next(new Error(error));
-	}
+  try {
+    const user = await User.find({ email: req.body.email });
+    const [{ data }] = user;
+    res.status(200);
+    res.json(data);
+  } catch (error) {
+    next(new Error(error));
+  }
 };
 
-export const insertCode = async (req, res, next) => {
-	const code = new Code({
-		dataId: req.body.dataId,
-		title: req.body.title,
-		code: req.body.code,
-		tags: req.body.tags,
-	});
-	res.status(200);
-	res.json({ message: "code saved" });
-	try {
-		await code.save();
-	} catch (error) {
-		next(new Error(error));
-	}
+export const updateCode = async (req, res, next) => {
+  try {
+    console.log();
+    const response = await Code.findOneAndUpdate(
+      { dataId: req.body.code.dataId },
+      {
+        title: req.body.code.title,
+        code: req.body.code.code,
+        tags: req.body.code.tags,
+      },
+      { new: true }
+    );
+    console.log(response);
+    res.status(200);
+    res.json({ message: "code saved" });
+  } catch (error) {
+    next(new Error(error));
+  }
 };
 
 export const getCode = async (req, res, next) => {
-	const { id } = req.params;
-	try {
-		const response = await Code.find({ dataId: id });
-		console.log("id ===> ", id);
-		console.log("response ===>", response);
-		res.status(200);
-		res.json({ data: response });
-	} catch (error) {
-		next(new Error(error));
-	}
+  const { id } = req.params;
+  try {
+    const response = await Code.find({ dataId: id });
+    res.status(200);
+    res.json({ data: response });
+  } catch (error) {
+    next(new Error(error));
+  }
+};
+
+export const createFolder = async (req, res, next) => {
+  const data = req.body;
+  try {
+    await User.findOneAndUpdate(
+      { email: data.email },
+      { $set: { "data.files": data.data } }
+    );
+    res.status(200);
+    res.json({ message: "user updated" });
+  } catch (error) {
+    next(new Error(error));
+  }
+};
+
+export const createFile = async (req, res, next) => {
+  const data = req.body;
+  const dataId = data.dataId;
+  const title = data.title;
+  console.log(dataId);
+
+  const code = new Code({
+    dataId,
+    title,
+    code: "### Title",
+    tags: [],
+  });
+
+  try {
+    await User.findOneAndUpdate(
+      { email: data.email },
+      { $set: { "data.files": data.data } }
+    );
+
+    await code.save();
+    res.status(200);
+    res.json({ message: "user updated" });
+  } catch (error) {
+    next(new Error(error));
+  }
 };
--- src/router/editorRouter.mjs
+++ src/router/editorRouter.mjs
@@ -1,9 +1,18 @@
 import express from "express";
 const router = express.Router();
 
-import { getData, getCode } from "../controllers/mainController.mjs";
+import {
+  getData,
+  getCode,
+  createFolder,
+  createFile,
+  updateCode,
+} from "../controllers/mainController.mjs";
 
 router.post("/data", getData);
 router.get("/code/:id", getCode);
+router.patch("/code/update", updateCode);
+router.post("/folders", createFolder);
+router.post("/files", createFile);
 
 export default router;
